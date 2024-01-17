3ae628326ba00a173653ac677a852b49dd7dba5a
Hapyy2 <58529384+Hapyy2@users.noreply.github.com>
2024-01-17 09:10:19 +0100

    Delete bubble_test.py

--- bubble_test.py
+++
@@ -1,4 +0,0 @@
-import bubble
-def test_bubble():
-    assert sorted([5, 4, 6, 7, 2, 3, 1]) == bubble.sort([5, 4, 6, 7, 2, 3, 1])
-    assert sorted([5, 4, 3, 1, 3, 2, 3]) == bubble.sort([5, 4, 3, 1, 3, 2, 3])
